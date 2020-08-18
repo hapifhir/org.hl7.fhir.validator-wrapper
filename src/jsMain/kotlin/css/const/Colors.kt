12d0d97301c3e92fd55cdaa8454470202b0350a7
markiantorno <markiantorno@gmail.com>
2020-08-18 16:06:54 -0400

    big ui cleanup, almost ready for merging v1

---
+++ src/jsMain/kotlin/css/const/Colors.kt
@@ -0,0 +1,21 @@
+package css
+
+import kotlinx.css.Color
+
+val REALLY_RED = Color("#B00000FF")
+val TRULY_RED = Color("#EB2229FF")
+val ALMOST_RED = Color("#FF6153FF")
+
+val BLACK = Color("#000000")
+val WHITE = Color("#FFFFFF")
+
+val GRAY_900 = Color("#212121")
+val GRAY_800 = Color("#424242")
+val GRAY_700 = Color("#616161")
+val GRAY_600 = Color("#757575")
+val GRAY_500 = Color("#9E9E9E")
+val GRAY_400 = Color("#BDBDBD")
+val GRAY_300 = Color("#E0E0E0")
+val GRAY_200 = Color("#EEEEEE")
+val GRAY_100 = Color("#F5F5F5")
+
