854f3aa7445182d9d3eccb3245b3b0c892d0354f
Keith Carlson <imkacarlson@gmail.com>
2020-09-01 09:11:32 -0400

    Initial basic test setup

---
+++ src/commonMain/kotlin/i18n/i18n.kt
@@ -0,0 +1,5 @@
+package i18n
+
+fun number(): Int {
+    return 5;
+}
---
+++ src/jvmTest/kotlin/i18nTest.kt
@@ -0,0 +1,8 @@
+import kotlin.test.*
+
+class i18nTest {
+    @Test
+    fun testNumber() {
+        assertEquals(i18n.number(), 5)
+    }
+}
