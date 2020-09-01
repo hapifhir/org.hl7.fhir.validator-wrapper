45ea5a0d1c1dcbcdd3ad35cefb2f5f1fa6b5f260
Keith Carlson <imkacarlson@gmail.com>
2020-09-01 09:52:16 -0400

    Add properties files and basic tests

--- src/commonMain/kotlin/i18n/i18n.kt
+++ src/commonMain/kotlin/i18n/i18n.kt
@@ -1,5 +1,17 @@
 package i18n
 
-fun number(): Int {
-    return 5;
+import java.util.*
+
+var currentLocale = Locale("en", "US")
+var messages = ResourceBundle.getBundle("MessagesBundle", currentLocale)
+
+fun setLocale(language: String, country: String) {
+    currentLocale = Locale(language, country)
+    messages = ResourceBundle.getBundle("MessagesBundle", currentLocale)
 }
+
+fun getMessage(id: String): String {
+    return messages.getString(id)
+}
+
+fun number(): Int = 5
--- src/jvmTest/kotlin/i18nTest.kt
+++ src/jvmTest/kotlin/i18nTest.kt
@@ -1,8 +1,23 @@
 import kotlin.test.*
 
+import java.text.MessageFormat
+
 class i18nTest {
+    val name = "James"
+    val year = "2020"
+
     @Test
-    fun testNumber() {
-        assertEquals(i18n.number(), 5)
+    fun testEnglish() {
+        i18n.setLocale("en", "US")
+        assertEquals(i18n.getMessage("GREETING"), "Hello World!")
+
+        assertEquals(MessageFormat.format(i18n.getMessage("PERSONAL_GREETING"), name, year),
+        MessageFormat.format("Hello {0}, the current year is {1}", name, year))
+    }
+
+    @Test
+    fun testGerman() {
+        i18n.setLocale("de", "DE")
+        assertEquals(i18n.getMessage("GREETING"), "Hallo Welt!")
     }
 }
