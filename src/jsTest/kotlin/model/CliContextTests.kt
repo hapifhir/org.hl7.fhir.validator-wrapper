836a0174a77a0390ceba6ed8258d53b8bb2a6cfe
markiantorno <markiantorno@gmail.com>
2021-03-08 10:44:45 -0500

    removing the one front end test that exists and will put a full test suite back in when I do session ids

--- src/jsTest/kotlin/model/CliContextTests.kt
+++ src/jsTest/kotlin/model/CliContextTests.kt
@@ -6,30 +6,30 @@ import kotlin.test.assertTrue
 
 class CliContextTests {
 
-    val testIg1 = "testIg1"
-    val testIg2 = "testIg2"
-    val testIg3 = "testIg3"
-
-    @Test
-    fun addIgTest() {
-        val cliContext = CliContext()
-        assertTrue(cliContext.getIgs().isEmpty())
-        cliContext.addIg(testIg1)
-        assertTrue(cliContext.getIgs().contains(testIg1))
-        cliContext.addIg(testIg2)
-        assertTrue(cliContext.getIgs().contains(testIg1))
-        assertTrue(cliContext.getIgs().contains(testIg2))
-    }
-
-    @Test
-    fun removeIgTest() {
-        val cliContext = CliContext()
-        assertTrue(cliContext.getIgs().isEmpty())
-        cliContext.addIg(testIg1)
-        cliContext.addIg(testIg2)
-        cliContext.removeIg(testIg1)
-        assertFalse(cliContext.getIgs().contains(testIg1))
-        assertTrue(cliContext.getIgs().contains(testIg2))
-    }
+//    val testIg1 = "testIg1"
+//    val testIg2 = "testIg2"
+//    val testIg3 = "testIg3"
+//
+//    @Test
+//    fun addIgTest() {
+//        val cliContext = CliContext()
+//        assertTrue(cliContext.getIgs().isEmpty())
+//        cliContext.addIg(testIg1)
+//        assertTrue(cliContext.getIgs().contains(testIg1))
+//        cliContext.addIg(testIg2)
+//        assertTrue(cliContext.getIgs().contains(testIg1))
+//        assertTrue(cliContext.getIgs().contains(testIg2))
+//    }
+//
+//    @Test
+//    fun removeIgTest() {
+//        val cliContext = CliContext()
+//        assertTrue(cliContext.getIgs().isEmpty())
+//        cliContext.addIg(testIg1)
+//        cliContext.addIg(testIg2)
+//        cliContext.removeIg(testIg1)
+//        assertFalse(cliContext.getIgs().contains(testIg1))
+//        assertTrue(cliContext.getIgs().contains(testIg2))
+//    }
 
 }
