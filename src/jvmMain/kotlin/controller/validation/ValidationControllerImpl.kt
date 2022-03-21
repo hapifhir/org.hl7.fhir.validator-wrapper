bf59f8da026292f86ab0088ff8af458cb68bdf94
dotasek <david.otasek@smilecdr.com>
2022-03-21 17:54:13 -0400

    Get core version from properties file

--- src/jvmMain/kotlin/controller/validation/ValidationControllerImpl.kt
+++ src/jvmMain/kotlin/controller/validation/ValidationControllerImpl.kt
@@ -5,6 +5,9 @@ import org.hl7.fhir.validation.cli.model.ValidationRequest
 import org.hl7.fhir.validation.cli.services.ValidationService
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
 
 class ValidationControllerImpl : ValidationController, KoinComponent {
 
@@ -12,5 +15,12 @@ class ValidationControllerImpl : ValidationController, KoinComponent {
 
     override suspend fun validateRequest(validationRequest: ValidationRequest): ValidationResponse {
         return validationService.validateSources(validationRequest)
+    }
+
+    override suspend fun getValidatorVersion():String {
+        val prop = Properties().apply {
+            load(FileInputStream(File("src/jvmMain/resources","app.properties")))
+        }
+        return prop.get("fhirCoreVersion").toString()
     }
 }
