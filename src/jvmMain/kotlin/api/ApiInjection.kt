5f147077d87cbbe3da87783fc020c12da434f525
dotasek <david.otasek@smilecdr.com>
2022-07-26 15:35:22 -0400

    Re-instantiate ValidationService once low memory threshold passed

--- src/jvmMain/kotlin/api/ApiInjection.kt
+++ src/jvmMain/kotlin/api/ApiInjection.kt
@@ -4,6 +4,8 @@ import api.terminogy.TerminologyApi
 import api.terminogy.TerminologyApiImpl
 import api.uptime.EndpointApi
 import api.uptime.EndpointApiImpl
+import controller.validation.ValidationServiceFactory
+import controller.validation.ValidationServiceFactoryImpl
 import org.hl7.fhir.utilities.npm.PackageClient
 import org.hl7.fhir.validation.cli.services.ValidationService
 import org.koin.dsl.module
@@ -13,7 +15,7 @@ object ApiInjection {
     private const val PACKAGE_CLIENT_ADDRESS = "https://packages.fhir.org"
 
     val koinBeans = module {
-        single<ValidationService> { ValidationService() }
+        single<ValidationServiceFactory> { ValidationServiceFactoryImpl() }
         single<PackageClient> { PackageClient(PACKAGE_CLIENT_ADDRESS) }
         single<TerminologyApi> { TerminologyApiImpl() }
         single<EndpointApi> { EndpointApiImpl() }
