7ef9a3bccd641183b9ffcaf0a7845bf9c6b8fff1
dotasek <david.otasek@smilecdr.com>
2022-10-04 17:31:59 -0400

    Fix PolyglotApi

--- src/jsMain/kotlin/api/PolyglotApi.kt
+++ src/jsMain/kotlin/api/PolyglotApi.kt
@@ -3,9 +3,11 @@ package api
 
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import kotlinx.js.Object
+import kotlinx.serialization.json.JsonObject
 
 
-suspend fun getPolyglotPhrases(): String {
-    val myBody : String = jsonClient.get(urlString = endpoint + "polyglot/en_US.json").body()
+suspend fun getPolyglotPhrases(): JsonObject {
+    val myBody : JsonObject = jsonClient.get(urlString = endpoint + "polyglot/en_US.json").body()
     return myBody
 }
--- src/jsMain/kotlin/reactredux/thunk/FetchPolyglotThunk.kt
+++ src/jsMain/kotlin/reactredux/thunk/FetchPolyglotThunk.kt
@@ -2,7 +2,6 @@ package reactredux.thunk
 
 import Polyglot
 import kotlinx.coroutines.*
-import kotlinx.browser.window
 import reactredux.slices.LocalizationSlice
 import reactredux.store.AppState
 import reactredux.store.RThunk
@@ -11,21 +10,29 @@ import redux.RAction
 import redux.WrapperAction
 
 import api.getPolyglotPhrases
-import kotlinext.js.asJsObject
 
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlin.js.json
+
 class FetchPolyglotThunk : RThunk {
 
 
     override fun invoke(dispatch: (RAction) -> WrapperAction, getState: () -> AppState): WrapperAction {
 
         GlobalScope.launch {
-            val phrases : String = getPolyglotPhrases()
-            console.log("hello fetchPolyglot")
-            console.log("phrases: " + phrases)
-            var polyglot = Polyglot(js("{locale: \"es\"}"))
-           polyglot.extend(phrases = JSON.parse(phrases))
+            val phrases : JsonObject = getPolyglotPhrases()
 
+            /*  Polyglot expects js or json as its phrases, so we need to convert our
+                kotlin JsonObject into the Pair<String, Any?> structure that the json
+                method will accept.
+             */
+            val pairs : Array<Pair<String, Any?>> = phrases.entries.map {
+                   it -> Pair(it.key, (it.value as JsonPrimitive).content)
+            }.toTypedArray()
+
+            var polyglot = Polyglot(js("{locale: \"en\"}"))
+            polyglot.extend(phrases = json(*pairs))
 
             dispatch(LocalizationSlice.SetPolyglot(polyglot))
         }
