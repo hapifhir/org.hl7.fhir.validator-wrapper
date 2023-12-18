662990e9f8cd9233c810d12784eddae18f1f5445
rpassas <35311744+rpassas@users.noreply.github.com>
2023-12-18 10:43:35 -0500

    Session config (#158)
    * session expiration configurable
    
    * create service function
    
    * adding PassiveExpiringSessionCache
    
    * Match renamed method
    
    * Bump to current core SNAPSHOT
    
    ---------
    
    Co-authored-by: dotasek <dotasek.dev@gmail.com>

--- src/jvmMain/kotlin/controller/validation/ValidationServiceFactoryImpl.kt
+++ src/jvmMain/kotlin/controller/validation/ValidationServiceFactoryImpl.kt
@@ -1,19 +1,31 @@
 package controller.validation
 
+import java.util.concurrent.TimeUnit;
 
 import org.hl7.fhir.validation.cli.services.ValidationService
+import org.hl7.fhir.validation.cli.services.SessionCache
+import org.hl7.fhir.validation.cli.services.PassiveExpiringSessionCache
 
 private const val MIN_FREE_MEMORY = 250000000
+private const val SESSION_DEFAULT_DURATION: Long = 60
 
 class ValidationServiceFactoryImpl : ValidationServiceFactory {
+    private var validationService: ValidationService
 
-    private var validationService = ValidationService();
+    init {
+        validationService = createValidationServiceInstance();
+    }
 
+     fun createValidationServiceInstance() : ValidationService {
+        val sessionCacheDuration = System.getenv("SESSION_CACHE_DURATION")?.toLong() ?: SESSION_DEFAULT_DURATION;
+        val sessionCache: SessionCache = PassiveExpiringSessionCache(sessionCacheDuration, TimeUnit.MINUTES).setResetExpirationAfterFetch(true);
+        return ValidationService(sessionCache);
+    }
+   
     override fun getValidationService() : ValidationService {
-
         if (java.lang.Runtime.getRuntime().freeMemory() < MIN_FREE_MEMORY) {
             println("Free memory ${java.lang.Runtime.getRuntime().freeMemory()} is less than ${MIN_FREE_MEMORY}. Re-initializing validationService");
-            validationService = ValidationService();
+            validationService = createValidationServiceInstance();
         }
         return validationService;
     }
