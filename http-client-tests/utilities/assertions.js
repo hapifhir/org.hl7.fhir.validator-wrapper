d5728346da26aede6cb5b0f81f980a4ac3d0e378
dotasek.dev <dotasek.dev@gmail.com>
2024-01-31 16:25:40 -0500

    Start test cases

---
+++ http-client-tests/utilities/assertions.js
@@ -0,0 +1,25 @@
+/**
+ * Blocks the execution thread for the at least the specified time in milliseconds.
+ *
+ * Analogous to Java Thread.sleep
+ * @param issues
+ * @param line
+ * @param col
+ * @param message
+ * @param type
+ * @param level
+ */
+export function containsIssue(issues, line, col, message, type, level) {
+    for (let index in issues) {
+
+        if (issues[index].line === line
+            && issues[index].col === col
+            && issues[index].message === message
+            && issues[index].type === type
+            && issues[index].level === level
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
